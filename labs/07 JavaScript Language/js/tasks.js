d4b9332c96ff13620349ae606e663c1fa1f26342
Diftraku <diftraku@gmail.com>
2016-03-23 15:30:55 +0200

    Worksheet 07

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -12,24 +12,13 @@ function addTask() {
     // Get the task:
     var task = document.getElementById('task');
 
-    // Reference to where the output goes:
-    var output = document.getElementById('output');
-    
-    // For the output:
-    var message = '';
-
     if (task.value) {
     
         // Add the item to the array:
         tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
+
+	    // Refactored drawing the task list to separate function
+        drawTaskList();
         
     } // End of task.value IF.
 
@@ -38,9 +27,33 @@ function addTask() {
     
 } // End of addTask() function.
 
+function drawTaskList() {
+	// Reference to where the output goes:
+	var output = document.getElementById('output');
+
+	// Update the page:
+	var message = '<h2>To-Do</h2><ol>';
+	for (var i = 0, count = tasks.length; i < count; i++) {
+		message += '<li>' + tasks[i] + '</li>';
+	}
+	message += '</ol>';
+	output.innerHTML = message;
+}
+
+function removeDuplicates() {
+	// @see http://stackoverflow.com/a/16747921
+	tasks = tasks.filter(function(elem, index, self) {
+		return index == self.indexOf(elem);
+	});
+
+	// Redraw the task list
+	drawTaskList();
+}
+
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('dedupe').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;
