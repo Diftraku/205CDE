62771226a4a3dbe02de1133b16f3526c2bf29c9d
Diftraku <diftraku@gmail.com>
2016-04-19 21:28:41 +0300

    Worksheet 19

--- labs/19 Document Databases/php/Persons.php
+++ labs/19 Document Databases/php/Persons.php
@@ -3,30 +3,48 @@
 require(APPPATH.'libraries/REST_Controller.php');
 
 class Persons extends REST_Controller {
-	
-	// GET method implementation for .../Persons/list
-	function list_get() {
 
-		// establish connection to MongoDB server
-		$connection = new MongoClient();
+    // GET method implementation for .../Persons/list
+    function list_get() {
 
-		// select database
-		$db = $connection->company;
+        // establish connection to MongoDB server
+        $connection = new MongoClient();
 
-		// select collection 
-		$collection = $db->persons;
+        // select database
+        $db = $connection->company;
 
-		// retrieve all items in collection
-		$cursor = $collection->find();
+        // select collection
+        $collection = $db->persons;
 
-		if($cursor->hasNext())
-		{
-    		echo json_encode(iterator_to_array($cursor));
-		}	
-		
-		// close connection
-		$connection->close();
-	}
+        // retrieve all items in collection
+        $cursor = $collection->find();
+
+        if($cursor->hasNext())
+        {
+            $this->response(iterator_to_array($cursor));
+        }
+
+        // close connection
+        $connection->close();
+    }
+
+    // POST method implementation for .../Persons/add
+    function add_post() {
+
+        // establish connection to MongoDB server
+        $connection = new MongoClient();
+
+        // select database
+        $db = $connection->company;
+
+        // select collection
+        $collection = $db->persons;
+        $current_id = array_values(iterator_to_array($collection->find()->sort(['id' => -1])->limit(1)))[0]['id'];
+
+        $this->response($collection->insert(['id' => $current_id+1, 'first name' => $this->post('first_name'), 'last name' => $this->post('last_name'), 'salary' => intval($this->post('salary'))]));
+
+        // close connection
+        $connection->close();
+    }
 }
 
-?>
